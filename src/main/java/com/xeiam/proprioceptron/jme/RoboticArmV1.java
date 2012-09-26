5b0c56d4b7b4e9b2c66b341c157f900a2c274b66
timmolter <tim.molter@gmail.com>
2012-09-26 13:51:18 +0200

    two moving joints!

--- src/main/java/com/xeiam/proprioceptron/jme/RoboticArmV1.java
+++ src/main/java/com/xeiam/proprioceptron/jme/RoboticArmV1.java
@@ -25,6 +25,7 @@ import com.jme3.math.ColorRGBA;
 import com.jme3.math.Quaternion;
 import com.jme3.math.Vector3f;
 import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
 import com.jme3.scene.shape.Box;
 
 /**
@@ -36,7 +37,11 @@ public class RoboticArmV1 extends SimpleApplication implements AnalogListener {
   /** BulletAppState allows using bullet physics in an Application. */
   private BulletAppState bulletAppState;
 
-  Geometry blue;
+  Geometry section0;
+  Node pivot0;
+
+  Geometry section1;
+  Node pivot1;
 
   @Override
   public void simpleInitApp() {
@@ -50,33 +55,62 @@ public class RoboticArmV1 extends SimpleApplication implements AnalogListener {
 
     RoboticArmUtils.createWorld(rootNode, assetManager, bulletAppState.getPhysicsSpace());
 
-    /** create a blue box at coordinates (1,-1,1) */
-    Box box1 = new Box(new Vector3f(0, 1, 0), .1f, 1, .1f);
-    blue = new Geometry("Box", box1);
-    Material mat1 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-    mat1.setColor("Color", ColorRGBA.Blue);
-    blue.setMaterial(mat1);
-    blue.move(0, 0, 0);
+    Box box = new Box(new Vector3f(0, 1, 0), .1f, 1, .1f);
 
-    rootNode.attachChild(blue);
+    // ///////////////////////////////////
+
+    section0 = new Geometry("Box", box);
+    Material mat0 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+    mat0.setColor("Color", ColorRGBA.Blue);
+    section0.setMaterial(mat0);
+
+    pivot0 = new Node("pivot");
+
+    // ///////////////////////////////////
+
+    section1 = new Geometry("Box", box);
+    Material mat1 = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+    mat1.setColor("Color", ColorRGBA.Green);
+    section1.setMaterial(mat1);
+
+    pivot1 = new Node("pivot");
+
+    // ///////////////////////////////////
+    // pivot0.move(0, 0, 0);
+    //
+    // section1.move(0, 2, 0);
+    // pivot1.attachChild(section0);
+
+    rootNode.attachChild(pivot0);
+    pivot1.move(0, 2, 0);
+    pivot0.attachChild(pivot1);
+
+    pivot0.attachChild(section0);
+    pivot1.attachChild(section1);
 
     setupKeys();
   }
 
   private void setupKeys() {
 
-    inputManager.addMapping("Left", new KeyTrigger(KeyInput.KEY_O));
-    inputManager.addMapping("Right", new KeyTrigger(KeyInput.KEY_P));
-    inputManager.addListener(this, "Left", "Right");
+    inputManager.addMapping("Left0", new KeyTrigger(KeyInput.KEY_L));
+    inputManager.addMapping("Right0", new KeyTrigger(KeyInput.KEY_P));
+    inputManager.addMapping("Left1", new KeyTrigger(KeyInput.KEY_K));
+    inputManager.addMapping("Right1", new KeyTrigger(KeyInput.KEY_O));
+    inputManager.addListener(this, "Left0", "Right0", "Left1", "Right1");
   }
 
   @Override
   public void onAnalog(String binding, float value, float tpf) {
 
-    if (binding.equals("Left")) {
-      blue.rotate(0, 0, value * speed);
-    } else if (binding.equals("Right")) {
-      blue.rotate(0, 0, -1 * value * speed);
+    if (binding.equals("Left0")) {
+      pivot0.rotate(0, 0, value * speed);
+    } else if (binding.equals("Right0")) {
+      pivot0.rotate(0, 0, -1 * value * speed);
+    } else if (binding.equals("Left1")) {
+      pivot1.rotate(0, 0, value * speed);
+    } else if (binding.equals("Right1")) {
+      pivot1.rotate(0, 0, -1 * value * speed);
     }
   }
 
